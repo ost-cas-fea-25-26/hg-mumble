4678593276eca8f24716717dc667dcfd6fb95fb2
Patrick <patrickbrunner96@hotmail.ch>
2025-11-10 22:31:03 +0100

    feat: add header draft and some default styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import { Button } from 'hg-storybook'
+import Header from '@/components/Header/Header'
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="gradient">Button</Button>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className={'flex flex-1 items-center justify-center'}>
+        <Button variant="gradient">Button</Button>
+      </main>
     </div>
   )
 }
