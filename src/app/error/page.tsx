4b2729148c0b13185795bcb21b84ff80e854b023
Patrick <patrickbrunner96@hotmail.ch>
2025-11-14 14:57:51 +0100

    feat: authentication should work

---
+++ src/app/error/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <section
+      className={'bg-primary flex items-center justify-center text-red-500'}
+    >
+      Error Page
+    </section>
+  )
+}
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import { Button, Cross, Eye, Field, Input, Label, Mumble } from 'hg-storybook'
+import { signIn } from '@/lib/auth-client'
+
+export default function Login() {
+  return (
+    <section className={'bg-primary flex items-center justify-center'}>
+      <div
+        className={
+          'mb-24 flex h-fit w-fit flex-col items-center justify-center gap-2 rounded-md bg-white p-6'
+        }
+      >
+        <Mumble className={'text-primary'} />
+        <h1 className={'text-primary text-2xl font-bold'}>Login</h1>
+        <Button variant={'primary'} onClick={() => signIn()}>
+          Let's gooo!!!
+        </Button>
+      </div>
+    </section>
+  )
+}
