ab48502092411e4653e26c8fd1c32376f4b5e887
jannikjost <jannik.jost@clavisit.com>
2025-11-14 15:44:47 +0100

    feat: add swr feat: add sonner feat: add intl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 import { Button } from 'hg-storybook'
 import Header from '@/components/Header/Header'
+import { toast } from 'sonner'
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className={'flex flex-1 items-center justify-center'}>
-        <Button variant="gradient">Button</Button>
+        <Button variant="gradient" onClick={() => toast('sdfadfa')}>
+          Button
+        </Button>
       </main>
     </div>
   )
