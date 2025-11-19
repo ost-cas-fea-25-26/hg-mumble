e7bc00551cd6219b68dd064d25c372d2a42136d0
Patrick <patrickbrunner96@hotmail.ch>
2025-11-19 15:01:39 +0100

    fix: build

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,11 +1,11 @@
 import { MumblePostCreateRequest } from '@/common/types/MumbleApi.types'
 import { Api } from '@/mumble/api'
 
-export async function GET(_req: Request) {
+export async function GET() {
   return await new Api().posts.postsControllerList({}, { baseUrl: process.env.API_URL })
 }
 
-export async function POST(_req: Request, _res: Response) {
+export async function POST() {
   const data: MumblePostCreateRequest = {}
   return await new Api().posts.postsControllerCreate(data)
 }
