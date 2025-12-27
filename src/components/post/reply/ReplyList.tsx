f45eb7ae9cdd83af86e0b38cfb4eec22ca8d29f4
jannikjost <jannik.jost@clavisit.com>
2025-12-27 09:39:20 +0100

    fix: refactor code and create postCreated Hook

--- src/components/post/reply/ReplyList.tsx
+++ src/components/post/reply/ReplyList.tsx
@@ -1,41 +1,18 @@
 'use client'
 
 import Reply from '@/components/post/reply/Reply'
-import React, { useEffect, useState } from 'react'
-import { toast } from 'sonner'
-import { useTranslations } from 'use-intl'
-import { Post as MumblePost, Reply as ReplyType } from '@/mumble/api/generated/MumbleApi'
+import usePostCreated from '@/hooks/usePostCreated'
+import React, { useState } from 'react'
+import { Reply as ReplyType } from '@/mumble/api/generated/MumbleApi'
 
 interface Props {
   initialReplies: ReplyType[]
 }
 
-function getPostEventSource() {
-  return new EventSource(`${process.env.NEXT_PUBLIC_API_URL}/posts/_sse`)
-}
-
 export default function ReplyList({ initialReplies }: Props) {
   const [replies, setReplies] = useState<ReplyType[]>(initialReplies)
-  const translate = useTranslations('mumble-reply')
 
-  const onPostCreated = (event: MessageEvent<string>) => {
-    const reply = JSON.parse(event.data) as MumblePost
-    setReplies((prev) => [reply, ...prev])
-    toast(translate('new-reply'), {
-      action: {
-        label: translate('see-now'),
-        onClick: () => window.scrollTo(0, 0),
-      },
-    })
-  }
-
-  useEffect(() => {
-    const events = getPostEventSource()
-    events.addEventListener('postCreated', onPostCreated)
-    return () => {
-      events.close()
-    }
-  }, [])
+  usePostCreated(setReplies, replies)
 
   return (
     <div>
---
+++ src/hooks/usePostCreated.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { toast } from 'sonner'
+import { useTranslations } from 'use-intl'
+import { Post as MumblePost } from '@/mumble/api/generated/MumbleApi'
+
+function getPostEventSource() {
+  return new EventSource(`${process.env.NEXT_PUBLIC_API_URL}/posts/_sse`)
+}
+
+export default function usePostCreated(updatePosts: (newPost: MumblePost[]) => void, posts: MumblePost[]) {
+  const translate = useTranslations('mumble-post')
+
+  const onPostCreated = (event: MessageEvent<string>) => {
+    const reply = JSON.parse(event.data) as MumblePost
+    updatePosts([reply, ...posts])
+    toast(translate('new-posts'), {
+      action: {
+        label: translate('see-now'),
+        onClick: () => window.scrollTo(0, 0),
+      },
+    })
+  }
+
+  useEffect(() => {
+    const events = getPostEventSource()
+    events.addEventListener('postCreated', onPostCreated)
+    return () => {
+      events.close()
+    }
+  }, [])
+}
