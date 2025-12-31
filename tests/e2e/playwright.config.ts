aedc4843e72dfe705e5b2a635b59de11c73b4989
jannikjost <jannik.jost@clavisit.com>
2025-12-31 12:06:07 +0100

    fix: ci

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,39 +1,14 @@
-import { defineConfig, devices } from '@playwright/test'
-
-const envReuse = process.env.PLAYWRIGHT_REUSE_EXISTING
-const envReuseBool = envReuse === 'true'
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: 'tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry',
-    ignoreHTTPSErrors: true,
+    headless: true,
   },
-  projects: [
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
-  webServer: [
-    {
-      command: 'npm run mock',
-      url: 'http://localhost:4011',
-      reuseExistingServer: envReuse ? envReuseBool : true,
-      timeout: 120 * 1000,
-    },
-    {
-      command: 'npm run dev:mock',
-      url: 'http://localhost:3000',
-      reuseExistingServer: envReuse ? envReuseBool : !process.env.CI,
-      timeout: 120 * 1000,
-      stdout: 'pipe',
-    },
-  ],
+  webServer: {
+    command: 'npm run dev:mock',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
 })
