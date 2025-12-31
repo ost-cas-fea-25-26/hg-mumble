9d3d99947ec6d74c4cf404227f6b4a4fff98c2e8
jannikjost <jannik.jost@clavisit.com>
2025-12-31 13:22:16 +0100

    fix: ci

--- tests/e2e/basic.test.ts
+++ tests/e2e/basic.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test'
 
 test('test', async ({ page }) => {
   await page.goto(`http://localhost:3000/`)
-  await expect(page.getByText('Zeit zu Mumblen!1')).toBeVisible()
+  await expect(page.getByText('Zeit zu Mumblen!')).toBeVisible()
 })
