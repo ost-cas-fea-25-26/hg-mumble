f531a223fe933f4bc28b96b76bee463a5ae076a0
jannikjost <jannik.jost@clavisit.com>
2026-01-02 10:40:21 +0100

    fix: fix ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from '@playwright/test'
+import dotenv from 'dotenv'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 export default defineConfig({
   testDir: 'tests',
