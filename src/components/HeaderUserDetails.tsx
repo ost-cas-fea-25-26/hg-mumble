34bb62b559e8b323d84df65e606893ff86b0afbb
Patrick <patrickbrunner96@hotmail.ch>
2026-01-05 20:39:43 +0100

    feat: add simple PWA offline page

--- src/components/HeaderUserDetails.tsx
+++ src/components/HeaderUserDetails.tsx
@@ -1,4 +1,5 @@
 import HeaderButtons from '@/components/HeaderButtons'
+import NetworkStatusManager from '@/components/NetworkStatusManager'
 import { getSession } from '@/lib/auth'
 import { Avatar, Link } from '@/lib/hg-storybook'
 import { getTranslations } from 'next-intl/server'
@@ -16,6 +17,7 @@ export default async function HeaderUserDetails() {
         <div className={'flex items-center justify-between gap-2'}>
           {sessionData!.user.image && <Avatar size={'s'} src={sessionData!.user.image} />}
           <HeaderButtons />
+          <NetworkStatusManager />
         </div>
       )}
     </>
