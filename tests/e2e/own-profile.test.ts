331a40ca7154fbaa7764a9284d4e42f6317accd7
Patrick <patrickbrunner96@hotmail.ch>
2026-01-08 18:16:53 +0100

    feat: add e2e fixture and add testcase

--- tests/e2e/own-profile.test.ts
+++ tests/e2e/own-profile.test.ts
@@ -1,4 +1,5 @@
-import test, { expect } from '@playwright/test'
+import { expect } from '@playwright/test'
+import test from './fixtures/mumble'
 
 test.describe('Own Profile Page', () => {
   test.beforeEach(async ({ page }) => {
