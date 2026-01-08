a563fc9f7ffc1804d3f44a0c82f25919984fa6ff
ckVendrix <clk.redox@gmail.com>
2026-01-08 19:46:01 +0100

    fix: lint

--- src/app/mumble/loading.tsx
+++ src/app/mumble/loading.tsx
@@ -2,11 +2,8 @@ import PageHeader from '@/components/general/PageHeader'
 import TabsSkeleton from '@/components/loading/TabsSkeleton'
 import PostSkeleton from '@/components/post/PostSkeleton'
 import CreatePostSkeleton from '@/components/post/create/CreatePostSkeleton'
-import { getTranslations } from 'next-intl/server'
 
 export default async function Loading() {
-  const translate = await getTranslations('general')
-
   return (
     <div className={'mt-6 flex flex-col gap-4'}>
       <PageHeader />
